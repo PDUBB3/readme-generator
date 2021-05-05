6f406a420b2f9fe5d4626965a622df906a9b805f
Pamela <pamela_bhopal@hotmail.co.uk>
2021-05-05 20:06:27 +0100

    template for function to generate markdown and questions created

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title for your application?",
+    name: "titleName",
+  },
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
 
 // TODO: Create a function to initialize app
-const init = () => {};
-
+const init = async () => {
+  const answers = await inquirer.prompt(questions);
+  console.log(answers);
+  const markdown = generateMarkdown(answers);
+};
 // Function call to initialize app
 init();
+console.log("js");
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,9 +11,47 @@ const renderLicenseLink = (license) => {};
 const renderLicenseSection = (license) => {};
 
 // TODO: Create a function to generate markdown for README
-const generateMarkdown = (data) => {
-  return `# ${data.title}
+const generateMarkdown = (answers) => {
+  return `# ${answers.titleName}
+  # Project Title (question 1 - input)
 
+  ![mit license](https://img.shields.io/badge/license-MIT-green) (question 2 - choices)
+  
+  ## Description
+  
+  ${answers.description}
+  
+  ## Table of Contents
+  
+    - [Project Title (question 1 - input)](#project-title-question-1---input)
+    - [Description](#description)
+    - [Table of Contents](#table-of-contents)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.applicationUsage}
+  ## License
+  ${answers.license}
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.testApplication}
+  
+  ## Questions
+  
+  - View my [GitHub](${answers.github}) profile
+  - Email me at ${answers.email} 
+  
 `;
 };
 
